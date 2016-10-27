8d3bb1ba36d32bd869001fb257c124f405fb2e75
xgqfrms <xgqfrms@outlook.com>
2016-10-27 21:52:29 +0800

    ok

---
+++ javatest/Welcome.java
@@ -0,0 +1,11 @@
+public class Welcome
+{
+  public static void main(String[] args)
+  {
+    String[] greeting = new String[2];
+    greeting[0] = "Welcome to Core Java";
+    greeting[1] = "by  xgqfrms"; 
+    for(String g : greeting)
+          System.out.println(g);
+  }
+}
